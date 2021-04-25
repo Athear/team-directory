b4e0b1ad1f897f889960ae6d9b45635a772d6e3a
Todd Malone <toddomalone@gmail.com>
2021-04-24 22:29:51 -0500

    corrected classname on header

--- src/components/DirectoryContainer.js
+++ src/components/DirectoryContainer.js
@@ -32,7 +32,7 @@ class DirectoryContainer extends Component {
   render() {
     return (
       <div>
-        <div class="header">
+        <div className="header">
           <h1>Employee Directory</h1>
           <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
         </div>
